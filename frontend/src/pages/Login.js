05246b97a613c7d8fb9ed209ffc6c56d5b0fefbe
Owen <osiemons@gmail.com>
2024-10-29 17:04:19 -0500

    Colors, better organization of styled components.

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -3,32 +3,76 @@ import {loginUrl} from './spotify'
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-
-const ButtonContainer = styled.div`
+const PageContainer = styled.div `
+    background-color: #e97451; 
     display: flex;
     justify-content: center;
     align-items: center;
     min-height: 100vh;
+    display: grid;
+    grid-template-columns: 1fr 2fr; // Two equal-width columns
+    gap: 200px; // Adjust spacing between columns as needed
+
+    @media (max-width: 768px) {
+        grid-template-columns: 1fr; // Single column on smaller screens
+    }
+`;
+
+const MainContainer = styled.div`
+    padding: 60px;
+
+`;
+
+const TitleContainer = styled.div `
+    padding: 15px 32px;
+    text-align: center;
+    font-size: 128px;
+    font-weight: bold;
+    text-shadow: 0 8px 16px rgba(0, 0, 0, 0.3);
+`;
+
+const ButtonContainer = styled.div`
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 
 const LoginButton = styled.button`
     background-color: #1DB954;
     border: none;
-    color: white;
     padding: 15px 32px;
     text-align: center;
-    font-size: 16px;
+    font-size: 18px;
+    font-weight: bold;
     cursor: pointer;
     border-radius: 32px;
+    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.4);
 `;
+
+
+const GraphicContainer = styled.div ``;
+
+
+
+
+
 
 const Login = () => {
     return (
-        <ButtonContainer>
-            <Link to="/graph">
-                <LoginButton>Login to Spotify</LoginButton>
-            </Link>
-        </ButtonContainer>
+        <PageContainer>
+            <MainContainer>
+                <TitleContainer>Mapify</TitleContainer>
+                <ButtonContainer>
+                    <Link to="/graph">
+                        <LoginButton>Login</LoginButton>
+                    </Link>
+                </ButtonContainer>
+            </MainContainer>
+
+            <GraphicContainer>THREE.JS MAGIC HERE</GraphicContainer>
+
+        </PageContainer>
     );
 };
 
