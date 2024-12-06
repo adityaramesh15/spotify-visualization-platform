6ba57909f194e9cc1a17e6da5b2df218372b0706
ahuss36 <ahuss36@illinois.edu>
2024-12-06 01:04:52 -0600

    Updated spotify.py to output dictionary

--- backend/spotify.py
+++ backend/spotify.py
@@ -12,16 +12,23 @@ class Spotify:
     def get_genre_durations(self):
         if not self.sp:
             raise ValueError("Spotify object not authenticated.")
-        
-        genre_durations = defaultdict(int)
+
         results = self.sp.current_user_top_tracks(time_range='long_term', limit=50)
-        for item in results['items']:
-            track = item
-            track_duration_ms = track['duration_ms']
-            artist_id = track['artists'][0]['id']
-            artist_info = self.sp.artist(artist_id)
 
-            for genre in artist_info['genres']:
-                genre_durations[genre] += track_duration_ms / 60000
+        # get track ID's and subsequent features
+        track_ids = [item['id'] for item in results['items']]
+        audio_features = self.sp.audio_features(track_ids)
 
-        return genre_durations
+        # dict keyed by (acousticness, energy), value as mins listened
+        acoustic_energy_map = {}
+        for item, features in zip(results['items'], audio_features):
+            track_duration_ms = item['duration_ms']
+            minutes_listened = track_duration_ms / 60000.0
+
+            acousticness = features['acousticness']
+            energy = features['energy']
+
+            # add to minutes listened if key is already in map, otherwise default to zero and add new value
+            acoustic_energy_map[(acousticness, energy)] = acoustic_energy_map.get((acousticness, energy), 0) + minutes_listened
+
+        return acoustic_energy_map
